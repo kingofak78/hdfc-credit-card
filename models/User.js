5a64e10adde987121cc170796e71325c4eb48bcb
kingofak78 <gotakale952@gmail.com>
2025-05-10 04:27:50 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,17 @@
+// models/User.js
 const mongoose = require('mongoose');
+
+const entrySchema = new mongoose.Schema({
+  name:        { type: String, required: true },
+  mobileNumber:{ type: String, required: true },
+  panNumber:   { type: String, required: true },
+  dob:         { type: String, required: true },
+  submittedAt: { type: Date,   default: Date.now }
+});
 
 const userSchema = new mongoose.Schema({
   uniqueid: { type: String, required: true, unique: true },
-  entries: [
-    {
-      name: { type: String, required: true },
-      mobileNumber: { type: String, required: true },
-      submittedAt: { type: Date, default: Date.now }
-    }
-  ]
+  entries:  [ entrySchema ]
 });
 
 module.exports = mongoose.model('User', userSchema);
